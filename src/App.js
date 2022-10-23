b7c141d366d19ce80e18badd9651d8106a63dd4c
AnandJNairGit <anandjnairit@gmail.com>
2022-10-23 19:06:10 +0530

    define all the page routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./pages/About";
+import Book from "./pages/Book";
+import BookList from "./pages/BookList";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
-   <h1>react routers</h1>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/book-list" element={<BookList />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
