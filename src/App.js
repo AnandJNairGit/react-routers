eeb7f622884f5c6152382a829aa627bba082749f
AnandJNairGit <anandjnairit@gmail.com>
2022-10-23 21:16:46 +0530

    add newBook routes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import Book from "./pages/Book";
 import BookList from "./pages/BookList";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import NewBook from "./pages/NewBook";
 
 function App() {
   return (
     <>
-    <nav>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/book-list">BookList</Link></li>
-
-
-      </ul>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/book-list">BookList</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/book/:id" element={<Book />} />
         <Route path="/book-list" element={<BookList />} />
+        <Route path="/book/:id" element={<Book />} />
+        <Route path="/book/new" element={<NewBook />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
